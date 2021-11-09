0c66890714334d3ac0c7da35d20a596af0bcbd52
Christian <christian.mf22@gmail.com>
2021-11-09 20:08:30 -0300

    Criado novas rotas

--- server.js
+++ server.js
@@ -9,8 +9,11 @@ const router = jsonServer.router("db.json");
 app.db = router.db;
 
 const rules = auth.rewriter({
-  users: 600,
+  users: 644,
   products: 644,
+	petDonation: 644,
+	profile: 644,
+	cart: 600
 });
 
 app.use(cors());
